24e22be3226cca33853c1df66e66a7ee756ec402
Victor Digital <victordj587@gmail.com>
2025-02-17 12:09:36 +0100

    Enhance annotation submission by adding blocking logic during submission to prevent duplicate requests

--- src/routes/lecture/[id]/Annotator.svelte
+++ src/routes/lecture/[id]/Annotator.svelte
@@ -6,22 +6,31 @@
 
 	let { data }: { data: PageData } = $props();
 
+	let blockSubmission = $state(false);
+
 	let timestampText = $state('');
 
 	async function handleSubmit(event: Event) {
-		event.preventDefault();
-		const res = await fetch(`/api/lecture/${data.lecture.id}/annotate`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ text: timestampText })
-		});
-		if (!res.ok) {
-			console.error('Failed to submit annotation:', res.statusText);
-			return;
+		blockSubmission = true;
+		try {
+			event.preventDefault();
+			const res = await fetch(`/api/lecture/${data.lecture.id}/annotate`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ text: timestampText })
+			});
+			if (!res.ok) {
+				console.error('Failed to submit annotation:', res.statusText);
+				return;
+			}
+			blockSubmission = false;
+			console.log('Annotation submitted successfully.');
+		} catch (error) {
+			blockSubmission = false;
+			console.error('Failed to submit annotation:', error);
 		}
-		console.log('Annotation submitted successfully.');
 	}
 </script>
 
@@ -29,10 +38,20 @@
 	<div class="flex max-w-xl flex-row flex-wrap items-end gap-2">
 		<div>
 			<Label for="text" class="mb-1">Timestamp</Label>
-			<Input placeholder="Timestamp text" type="text" name="text" bind:value={timestampText} />
+			<Input
+				disabled={blockSubmission}
+				placeholder="Timestamp text"
+				type="text"
+				name="text"
+				bind:value={timestampText}
+			/>
 		</div>
 		<div>
-			<button type="submit" class={buttonVariants({ variant: 'default' })}>Submit</button>
+			<button
+				disabled={blockSubmission}
+				type="submit"
+				class={buttonVariants({ variant: 'default' })}>Submit</button
+			>
 		</div>
 	</div>
 </form>
